8eae36102b24da1473dc3eea55a1f007547592c3
hudney-fsbrito <hudney.rse@gmail.com>
2023-03-01 22:02:34 -0300

    add botão excluir

--- js/script.js
+++ js/script.js
@@ -5,22 +5,23 @@ let listaTarefa = document.querySelector("#lista-Tarefa");
 
 
 inputNovaTarefa.addEventListener("keypress", (e) => {
-    
-    if(e.keyCode == 13){
-       let tarefa = {
+
+    if (e.keyCode == 13) {
+        let tarefa = {
             nome: inputNovaTarefa.value,
-            id:gerarId(),
-       }
-       criarTarefa(tarefa)
+            id: gerarId(),
+        }
+        console.log(tarefa);
+        criarTarefa(tarefa)
     }
 });
 addTarefa.addEventListener("click", (e) => {
-    
-       let tarefa = {
-            nome: inputNovaTarefa.value,
-            id:gerarId(),
-       }
-       criarTarefa(tarefa)
+
+    let tarefa = {
+        nome: inputNovaTarefa.value,
+        id: gerarId(),
+    }
+    criarTarefa(tarefa)
 });
 
 function gerarId() {
@@ -35,26 +36,45 @@ function criarTarefa(tarefa) {
 
 function criarTagLI(tarefa) {
     let li = document.createElement('li');
+    li.id = tarefa.id;
 
     let span = document.createElement('span');
     span.classList.add('nome-Tarefa');
     span.innerHTML = tarefa.nome;
-    
+
     let div = document.createElement('div');
     div.classList.add('container-tarefas');
 
     let btnEditar = document.createElement("button");
     btnEditar.classList.add("edita-Tarefa");
     btnEditar.innerHTML = '<i class="fa fa-pencil"></i>';
+    btnEditar.setAttribute('onclick', 'editar(' + tarefa.id + ')');
 
     let btnApagar = document.createElement("button");
     btnApagar.classList.add("exclui-Tarefa");
     btnApagar.innerHTML = '<i class="fa fa-trash"></i>';
+    btnApagar.setAttribute('onclick', 'apagar(' + tarefa.id + ')');
 
     div.appendChild(btnEditar);
     div.appendChild(btnApagar);
 
     li.appendChild(span);
     li.appendChild(div);
+    console.log(tarefa.id);
     return li
 };
+
+function apagar(idTarefa) {
+    let confirm = window.confirm(`Deseja excluir PERMANENTIMENTE a tarefa: ${idTarefa}`)
+    if (confirm) {
+        let li = document.getElementById('' + idTarefa + '')
+        if (li) {
+            listaTarefa.removeChild(li)
+        }
+
+    }
+
+}
+function editar(idTarefa) {
+    alert(idTarefa)
+}
